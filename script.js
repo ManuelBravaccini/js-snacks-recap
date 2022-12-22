db93867184b8c7528d4f26f509126fd8e644ae87
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-12-22 10:20:55 +0100

    Odd or even number with API

---
+++ script.js
@@ -0,0 +1,29 @@
+/*
+Creare due div; uno conterrà numeri dispari di colore rosso e l'altro conterrà numeri pari in verde.
+Ad ogni click di un bottone chiedere all'API un numero, se è dispari metterlo nel blocco dispari, e se è pari in quello pari.
+*/
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            numOdd: [],
+            numEven: [],
+        }
+    },
+
+    methods: {
+        getNewNum() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/int')
+            .then((response) => {
+                //console.log(response.data.response);
+                if (response.data.response % 2 == 0) {
+                    this.numEven.push(response.data.response);
+                } else {
+                    this.numOdd.push(response.data.response);
+                }
+            });
+        }
+    }
+}).mount('#app')
